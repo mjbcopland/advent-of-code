5d0dd01059500fe73758821728eee627f0db40d4
Michael Copland <mjbcopland@gmail.com>
2019-12-06 18:21:57 +0000

    Day 3 part 2 wip

--- 2019/day/3/solution.py
+++ 2019/day/3/solution.py
@@ -7,13 +7,15 @@ import itertools
 def get_coordinates(path):
     # start at the origin
     x, y = (0, 0)
+    dist = 0
 
     # include the starting coordinate
-    coordinates = [(x, y)]
+    coordinates = [((x, y), dist)]
 
     for step in path:
         direction = step[0]
         length = int(step[1:])
+        dist += length
 
         if direction == "U":
             y += length
@@ -24,7 +26,7 @@ def get_coordinates(path):
         if direction == "R":
             x += length
 
-        coordinates.append((x, y))
+        coordinates.append(((x, y), dist))
 
     return coordinates
 
@@ -47,16 +49,24 @@ def get_m_c(segment):
 
 
 def get_intersection(pair):
+    # (left, d1), (right, d2) = pair
+    ((a, d1), (b, _)), ((c, d2), (d, _)) = pair
+
     left, right = pair
 
+    (a, d1), (b, _) = left
+    (c, d2), (d, _) = right
+
+    # print(left, right)
+
     try:
-        m1, c1 = get_m_c(left)
+        m1, c1 = get_m_c((a, b))
     except ZeroDivisionError:
         # left segment is vertical
         return get_vertical_intersection(left, right)
 
     try:
-        m2, c2 = get_m_c(right)
+        m2, c2 = get_m_c((c, d))
     except ZeroDivisionError:
         # right segment is vertical
         return get_vertical_intersection(right, left)
@@ -65,7 +75,9 @@ def get_intersection(pair):
         x = (c2 - c1) / (m1 - m2)
         y = (m1 * x) + c1
 
-        return (x, y) if inside((x, y), left) and inside((x, y), right) else None
+        d = d1 + d2 + manhatten_distance(a, (x, y)) + manhatten_distance(c, (x, y))
+
+        return ((x, y), d) if inside((x, y), left) and inside((x, y), right) else None
 
     except ZeroDivisionError:
         # the segments are parallel so cannot intersect
@@ -73,21 +85,27 @@ def get_intersection(pair):
 
 
 def get_vertical_intersection(vert, other):
-    (xi, yi), (xj, yj) = vert
+    (v1, d1), (v2, _) = vert
+    (o1, d2), (o2, _) = other
+
+    (xi, _), (xj, _) = (v1, v2)
 
     assert xi == xj, "Segment is not vertical"
 
     x = xi
 
     try:
-        m, c = get_m_c(other)
+        m, c = get_m_c((o1, o2))
     except ZeroDivisionError:
         # both segments are vertical and cannot intersect
         return None
 
     y = (m * x) + c
+    d = d1 + d2 + manhatten_distance(v1, (x, y)) + manhatten_distance(o1, (x, y))
 
-    return (x, y) if inside((x, y), vert) and inside((x, y), other) else None
+    return (
+        ((x, y), d) if inside((x, y), (v1, v2)) and inside((x, y), (o1, o2)) else None
+    )
 
 
 def inside(coord, segment):
@@ -109,6 +127,7 @@ if __name__ == "__main__":
         paths = (line.strip().split(",") for line in file)
 
         coordinates = map(get_coordinates, paths)
+
         segments = map(get_segments, coordinates)
 
         intersections = []
@@ -118,7 +137,7 @@ if __name__ == "__main__":
             if intersection is not None:
                 intersections.append(intersection)
 
-        intersections.sort(key=lambda coord: manhatten_distance((0, 0), coord))
+        intersections.sort(key=lambda x: x[1])
 
         try:
             print(intersections[0])
